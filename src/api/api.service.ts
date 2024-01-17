408047d649c78820adc1bf844bbe22afb34277d5
Ranjan Gupta <ranjan.gupta@traderinteractive.com>
2024-01-17 15:24:40 +0530

    Initial Commit

---
+++ src/api/api.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { Student } from '../student/student.service';
+import {HttpService} from '@nestjs/axios';
+
+@Injectable()
+export class ApiService {
+  constructor(private http: HttpService) {}
+
+  async getStudent({ firstName, lastName }: { firstName: string; lastName: string; }): Promise<Student> {
+    const url = `../get-student?firstName=${firstName}&lastName=${lastName}`;
+    const response = await this.http.get(url).toPromise();
+    return response.data;
+  }
+
+  getHello(): string {
+    return 'Hello World!';
+  }
+}
